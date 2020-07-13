2ff6d8bf4e6a3b71364b098641659dd792eae123
eladyaniv01 <eladyaniv01@gmail.com>
2020-07-13 18:13:33 +0300

    * all constructs now inherit from Polygon Class * Polygon - init is now done with map_data and numpy.ndarray           - region is now a property, and will work for all constructs * MapData - added  points_to_numpy_array utility function * Region - now defines the polygon without passing self as region
    Signed-off-by: eladyaniv01 <eladyaniv01@gmail.com>

--- MapAnalyzer/constructs.py
+++ MapAnalyzer/constructs.py
@@ -1,19 +1,19 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Tuple
 
 import numpy as np
 from sc2.game_info import Ramp as sc2Ramp
-from scipy.ndimage import center_of_mass
+
+from MapAnalyzer.Polygon import Polygon
 
 if TYPE_CHECKING:
     from .MapData import MapData
 
 
-class ChokeArea:
-    def __init__(self, map_data: "MapData", points, main_line=None):
-        self.map_data = map_data
+class ChokeArea(Polygon):
+    def __init__(self, array: np.ndarray, map_data: "MapData", main_line: Tuple = None):
         self.regions = []  # set by map_data
-        self.points = points
         self.main_line = main_line
+        super().__init__(map_data=map_data, array=array)
 
     def get_width(self):
         import math
@@ -22,33 +22,13 @@ class ChokeArea:
             x2, y2 = self.main_line[1][0], self.main_line[1][1]
             return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-    @property
-    def indices(self):
-        return self.map_data.points_to_indices(self.points)
-
-    @property
-    def center(self):
-        cm = center_of_mass(self.array)
-        return np.int(cm[0]), np.int(cm[1])
-
-    @property
-    def array(self):
-        mask = np.zeros(self.map_data.region_grid.shape, dtype='int')
-        mask[self.indices] = 1
-        return mask
-
-    @property
-    def area(self):
-        return len(self.points)
-
     def __repr__(self):
         return f'<ChokeArea;{self.area}> of {[r for r in self.regions]}'
 
-
 class MDRamp(ChokeArea):
-    def __init__(self, map_data: "MapData", points, ramp: sc2Ramp):
+    def __init__(self, map_data: "MapData", array: np.ndarray, ramp: sc2Ramp):
         self.ramp = ramp
-        super().__init__(map_data=map_data, points=points)
+        super().__init__(map_data=map_data, array=array)
 
     @property
     def top_center(self):
@@ -63,8 +43,8 @@ class MDRamp(ChokeArea):
 
 
 class VisionBlockerArea(ChokeArea):
-    def __init__(self, map_data: "MapData", points):
-        super().__init__(map_data=map_data, points=points)
+    def __init__(self, map_data: "MapData", array: np.ndarray):
+        super().__init__(map_data=map_data, array=array)
 
     def __repr__(self):
         return f'<VisionBlockerArea;{self.area}> of {[r for r in self.regions]}'
