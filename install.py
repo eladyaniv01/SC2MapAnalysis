b67389d1ea8691764f22b9a86bbc67a4f985d53c
eladyaniv01 <eladyaniv01@gmail.com>
2020-07-31 02:25:42 +0300

    i think pyastar is now in
    Signed-off-by: eladyaniv01 <eladyaniv01@gmail.com>

--- install.py
+++ install.py
@@ -1,6 +1,5 @@
 import subprocess
 
 if __name__ == "__main__":
+    subprocess.call(f"pip install .", shell=True)
     subprocess.call(f"pip install ./mapyastar ", shell=True)
-    # subprocess.call(f"pip install .", shell=True)
-    # subprocess.call(f"pip install {os.path.join(os.getcwd(), 'mapyastar')}", shell=True)
