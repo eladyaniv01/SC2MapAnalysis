d51667542bcc31bafd5aaa5e0ff41aa50fd79cb5
eladyaniv01 <eladyaniv01@gmail.com>
2020-07-30 18:56:44 +0300

    added pyastar as a discovery submodule
    Signed-off-by: eladyaniv01 <eladyaniv01@gmail.com>

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup  # pragma: no cover
 
 requirements = [  # pragma: no cover
-
-        # "Cython",
-        # "burnysc2==4.11.16",
-        # "click==7.1.2",
-        # "matplotlib==3.2.2",
-        # "mpyq==0.2.5",
-        # "numpy==1.19.0",
-        # "scikit-image==0.17.2",
-        # "scipy==1.5.1",
-        # "six==1.15.0",
-        # "tifffile==2020.7.4",
-        # "yarl==1.4.2",
-        # "loguru==0.5.1",
-        # "tqdm"
+        "Cython",
+        "burnysc2==4.11.16",
+        "click==7.1.2",
+        "matplotlib==3.2.2",
+        "mpyq==0.2.5",
+        "numpy==1.19.0",
+        "scikit-image==0.17.2",
+        "scipy==1.5.1",
+        "six==1.15.0",
+        "tifffile==2020.7.4",
+        "yarl==1.4.2",
+        "loguru==0.5.1",
+        "tqdm"
 
 ]
 setup(  # pragma: no cover
