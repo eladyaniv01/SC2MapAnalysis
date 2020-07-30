12f136eb3a15a9f0f8ea6065b6271f8060f0fcd7
eladyaniv01 <eladyaniv01@gmail.com>
2020-07-31 01:49:36 +0300

    added submodule git file
    Signed-off-by: eladyaniv01 <eladyaniv01@gmail.com>

---
+++ install.py
@@ -0,0 +1,6 @@
+import os
+import subprocess
+
+if __name__ == "__main__":
+    subprocess.call(f"pip install .", shell=True)
+    subprocess.call(f"pip install {os.path.join(os.getcwd(), 'pyastar')}", shell=True)
