23b9ed3a551c947b483371265ffe3bd4f79d8946
eladyaniv01 <40754127+eladyaniv01@users.noreply.github.com>
2020-08-03 21:06:41 +0300

    More tests (#47)
    * test regions works for all points
    
    * fix: where bug not querying pure chokes
    all logger warnings are now debug
    added mocksetup.py for test mock setup,  and also one place for imports , this file is excluded from inspection
    added test_pathing
    and moved path tests from test_suite to that
    folder structure change : added /tests  and moved all tests into it

---
+++ tests/mocksetup.py
@@ -0,0 +1,61 @@
+import logging
+import os
+import random
+from random import randint
+from typing import Iterable, List
+
+import pytest
+import tqdm
+from _pytest.python import Metafunc
+from hypothesis import given, settings, strategies as st
+from loguru import logger
+
+from MapAnalyzer.MapData import MapData
+from MapAnalyzer.Region import Region
+from MapAnalyzer.Polygon import Polygon
+from MapAnalyzer.constructs import ChokeArea, MDRamp, VisionBlockerArea
+from MapAnalyzer.utils import mock_map_data
+from _pytest.logging import caplog as _caplog
+
+def get_random_point(minx, maxx, miny, maxy):
+    return (random.randint(minx, maxx), random.randint(miny, maxy))
+
+@pytest.fixture
+def caplog(_caplog=_caplog):
+    class PropogateHandler(logging.Handler):
+        def emit(self, record):
+            logging.getLogger(record.name).handle(record)
+
+    handler_id = logger.add(PropogateHandler(), format="{message}")
+    yield _caplog
+    logger.remove(handler_id)
+
+
+def get_map_file_list() -> List[str]:
+    """
+    easy way to produce less than all maps,  for example if we want to test utils, we only need one MapData object
+    """
+    subfolder = "MapAnalyzer"
+    subfolder2 = "pickle_gameinfo"
+    subfolder = os.path.join(subfolder, subfolder2)
+    folder = os.path.abspath(".")
+    map_files_folder = os.path.join(folder, subfolder)
+    map_files = os.listdir(map_files_folder)
+    li = []
+    for map_file in map_files:
+        li.append(os.path.join(map_files_folder, map_file))
+    return li
+
+
+def get_map_datas() -> Iterable[MapData]:
+    subfolder = "MapAnalyzer"
+    subfolder2 = "pickle_gameinfo"
+    subfolder = os.path.join(subfolder, subfolder2)
+    if "tests" in os.path.abspath("."):
+        folder = os.path.dirname(os.path.abspath("."))
+    else:
+        folder = os.path.abspath(".")
+    map_files_folder = os.path.join(folder, subfolder)
+    map_files = os.listdir(map_files_folder)
+    for map_file in map_files:
+        yield mock_map_data(map_file=os.path.join(map_files_folder, map_file))
