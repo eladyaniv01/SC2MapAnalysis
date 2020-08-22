7333db0173b336eea2a113a86f37e3ef1b5f25d2
eladyaniv01 <eladyaniv01@gmail.com>
2020-08-22 18:23:34 +0300

    refactor: version attribute is no in MapAnalyzer.__init__.py ,  updated versionbump methods  in vb,  and adjusted setup

--- MapAnalyzer/__init__.py
+++ MapAnalyzer/__init__.py
@@ -4,5 +4,6 @@ from .MapData import MapData
 from .Polygon import Polygon
 from .Region import Region
 from .constructs import ChokeArea, MDRamp, VisionBlockerArea
+import numpy as np
 
 __version__ = "0.0.55"
