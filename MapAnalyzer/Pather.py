8995d15d0a450af54c5811859924a504c8c3ef27
Tom Kerr <tomkerrdev@gmail.com>
2020-09-18 11:19:07 +0100

    Excluded lowered supply depots from non pathables

--- MapAnalyzer/Pather.py
+++ MapAnalyzer/Pather.py
@@ -4,6 +4,7 @@ import numpy as np
 import pyastar.astar_wrapper as pyastar
 from numpy import ndarray
 from sc2.position import Point2
+from sc2.ids.unit_typeid import UnitTypeId as UnitID
 from skimage import draw as skdraw
 
 from MapAnalyzer.constants import NONPATHABLE_RADIUS_FACTOR, RESOURCE_BLOCKER_RADIUS_FACTOR, GEYSER_RADIUS_FACTOR
@@ -69,8 +70,8 @@ class MapAnalyzerPather:
         )
 
     def _add_non_pathables_ground(self, grid: ndarray, include_destructables: bool = True) -> ndarray:
-        nonpathables = self.map_data.bot.structures
-        nonpathables.extend(self.map_data.bot.enemy_structures)
+        nonpathables = self.map_data.bot.structures.exclude_type({UnitID.SUPPLYDEPOTLOWERED})
+        nonpathables.extend(self.map_data.bot.enemy_structures.exclude_type({UnitID.SUPPLYDEPOTLOWERED}))
         nonpathables.extend(self.map_data.mineral_fields)
         nonpathables.extend(self.map_data.normal_geysers)
         for obj in nonpathables:
