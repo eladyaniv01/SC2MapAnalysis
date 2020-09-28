b91efd3394f38d49ed16016a63764331c634b3b2
eladyaniv01 <eladyaniv01@gmail.com>
2020-09-28 20:18:17 +0300

    setup bump 0.0.71

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [  # pragma: no cover
 setup(  # pragma: no cover
         name="sc2mapanalyzer",
         # version=f"{__version__}",
-        version="0.0.70",
+        version="0.0.71",
         install_requires=requirements,
         setup_requires=["wheel", "numpy"],
         extras_require={
