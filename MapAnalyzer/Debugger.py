d9fe9971b8d056bf02840d1391dd862530c93e54
Elad <eladyaniv01@gmail.com>
2020-12-13 21:08:52 +0200

    feat: Debugger now plots choke side a and side b with text indicators (sA, sB)

--- MapAnalyzer/Debugger.py
+++ MapAnalyzer/Debugger.py
@@ -159,19 +159,19 @@ class MapAnalyzerDebugger:
                 plt.text(cm[0], cm[1], f"C<{choke.id}>", fontdict=fontdict,
                          bbox=dict(fill=True, alpha=0.3, edgecolor="red", linewidth=2))
                 plt.scatter(x, y, marker=r"$\heartsuit$", s=100, edgecolors="r", alpha=0.3)
-            walls = [choke.left, choke.right]
+            walls = [choke.side_a, choke.side_b]
             x, y = zip(*walls)
             fontdict = {"family": "serif", "size": 5}
             if 'unregistered' not in str(choke.id).lower():
-                plt.text(choke.left[0], choke.left[1], f"C<{choke.id}L>", fontdict=fontdict,
-                         bbox=dict(fill=True, alpha=0.3, edgecolor="green", linewidth=2))
-                plt.text(choke.right[0], choke.right[1], f"C<{choke.id}R>", fontdict=fontdict,
-                         bbox=dict(fill=True, alpha=0.9, edgecolor="red", linewidth=2))
+                plt.text(choke.side_a[0], choke.side_a[1], f"C<{choke.id}sA>", fontdict=fontdict,
+                         bbox=dict(fill=True, alpha=0.5, edgecolor="green", linewidth=2))
+                plt.text(choke.side_b[0], choke.side_b[1], f"C<{choke.id}sB>", fontdict=fontdict,
+                         bbox=dict(fill=True, alpha=0.5, edgecolor="red", linewidth=2))
             else:
-                plt.text(choke.left[0], choke.left[1], f"L>", fontdict=fontdict,
-                         bbox=dict(fill=True, alpha=0.3, edgecolor="green", linewidth=2))
-                plt.text(choke.right[0], choke.right[1], f"R>", fontdict=fontdict,
-                         bbox=dict(fill=True, alpha=0.9, edgecolor="red", linewidth=2))
+                plt.text(choke.side_a[0], choke.side_a[1], f"sA>", fontdict=fontdict,
+                         bbox=dict(fill=True, alpha=0.5, edgecolor="green", linewidth=2))
+                plt.text(choke.side_b[0], choke.side_b[1], f"sB>", fontdict=fontdict,
+                         bbox=dict(fill=True, alpha=0.5, edgecolor="red", linewidth=2))
             plt.scatter(x, y, marker=r"$\spadesuit$", s=50, edgecolors="b", alpha=0.5)
 
     def plot_map(
