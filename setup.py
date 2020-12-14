1c6b74ccc472de698db0a95a6b2baef433b92465
Elad <eladyaniv01@gmail.com>
2020-12-14 14:58:07 +0200

    feat: use custom implementation ofr pyastar
    downgrade numpy to 1.19.3  for windows users

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ logger = logging.getLogger(__name__)
 
 requirements = [  # pragma: no cover
         "wheel",
-        "numpy",
+        "numpy==1.19.3",
         "Cython",
-        "pyastar@git+git://github.com/hjweide/pyastar.git@master#egg=pyastar",
+        "pyastar@git+git://github.com/eladyaniv01/pyastar.git@master#egg=pyastar",
         "burnysc2",
         "matplotlib",
         "scipy",
@@ -21,7 +21,7 @@ setup(  # pragma: no cover
         # version=f"{__version__}",
         version="0.0.77",
         install_requires=requirements,
-        setup_requires=["wheel", "numpy"],
+        setup_requires=["wheel", "numpy==1.19.3"],
         extras_require={
                 "dev": [
                         "pytest",
